13a413eb50ec032ad8a1133d5907339ed574341c
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-08-22 00:33:23 +0530

    jsx

--- src/Components/Info2/Info.jsx
+++ src/Components/Info2/Info.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import './Info2.css';
 
 function Info2() {
-  return (
-    <section className="info2">
-      <div className="info2-content">
-        <h1 className="info2-heading">Your Trusted Partner</h1>
-        <h2 className="info2-subheading">We Are Here to Help</h2>
-        <button className="info2-button">Contact</button>
-      </div>
-    </section>
-  );
+    return (
+        <section className="info2">
+            <div className="info2-content">
+                <h1 className="info2-heading">Your Trusted Partner</h1>
+                <h2 className="info2-subheading">We Are Here to Help</h2>
+                <button className="info2-button">Contact</button>
+            </div>
+        </section>
+    );
 }
 
 export default Info2;
