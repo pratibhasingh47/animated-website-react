9bc25cd7fa6f2eb014e28d12e96055d01704742f
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-08-21 23:24:21 +0530

    jsx

--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './Info.css';
+import info from '../Images/info.jpg';
 
 function Info() {
     const [isVisible, setIsVisible] = useState(false);
@@ -22,7 +23,7 @@ function Info() {
     return (
         <section id="info-section" className={`info ${isVisible ? 'animate' : ''}`}>
             <div className="info-left">
-                <img src="path_to_image.jpg" alt="Info" />
+                <img src={info} alt="Info" />
             </div>
             <div className="info-right">
                 <h2 className="info-heading">Our Vision</h2>
