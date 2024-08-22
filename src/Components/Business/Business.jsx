0b0dd25ec5dad1f78d5fcd866b46a463831c58ec
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-08-23 00:02:34 +0530

    jsx

--- src/Components/Business/Business.jsx
+++ src/Components/Business/Business.jsx
@@ -26,7 +26,7 @@ function Business() {
                 <div className="business-left">
                     <h2 className="business-heading">We build adjacent businesses to optimize our areas of expertise</h2>
                     <p className="business-description">
-                    Elit sed vulputate mi sit amet mauris. Arcu odio ut sem nulla pharetra. Nisi scelerisque eu ultrices vitae. Dapibus ultrices in iaculis nunc sed augue lacus. Laoreet non curabitur gravida arcu ac. Eget velit aliquet sagittis id consectetur purus ut. Sit amet luctus venenatis lectus magna fringilla urna. Dolor morbi non arcu risus quis varius quam. Aenean sed adipiscing diam donec adipiscing tristique risus.
+                        Elit sed vulputate mi sit amet mauris. Arcu odio ut sem nulla pharetra. Nisi scelerisque eu ultrices vitae. Dapibus ultrices in iaculis nunc sed augue lacus. Laoreet non curabitur gravida arcu ac. Eget velit aliquet sagittis id consectetur purus ut. Sit amet luctus venenatis lectus magna fringilla urna. Dolor morbi non arcu risus quis varius quam. Aenean sed adipiscing diam donec adipiscing tristique risus.
                     </p>
                     <button className="business-button">Learn More</button>
                 </div>
